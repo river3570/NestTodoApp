08bd202fe9903cd5f00ed2199bb8b704808147bb
river3570 <R.riverJob1590@gmail.com>
2026-03-30 10:35:18 +0900

    ejsのインポートなど。

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,12 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable } from "@nestjs/common";
 
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello NestJS!';
+    return "Hello NestJS!";
+  }
+
+  getFood(food: string): string {
+    return `${food}!`;
   }
 }
